6f5fd5b68fa34dbd88ef74f9f5973392a3c0a2a6
Cory Wheeler <corywheeler@gmail.com>
2015-09-09 06:06:02 -0400

    Set up a Tracker.autorun to observe it's behavior. The function given to autorun will run immediately once at least when the app loads in the client due to it asking for the favorite food, which has been made into a reactive variable. It will run regardless of what route you are on.
    So if you visit http://localhost:3000 you will see it run once. This is
    because the app has loaded, the favoriteFood variable has been set, and
    a call to getFavoriteFood via the autorun has executed.
    
    And if you visit http://localhost:3000/simpleexample you will see it run
    once as well.
    
    I would have thought that the second one would not have run though as the
    variable had already been set. A little off in my mind. Curiouse what would
    happen if you visited some other route, what would happen?
    
    Flip some of the " to '
    
    Remove the isClient wrapper as this file is now in the client folder and will
    only run on the client now by convention.

--- client/favoriteFood/favoriteFood.js
+++ client/favoriteFood/favoriteFood.js
@@ -1,38 +1,43 @@
+var favoriteFood = 'apples';
+var favoriteFoodDep = new Tracker.Dependency;
 
-
-if (Meteor.isClient) {
-
-    var favoriteFood = "apples";
-    var favoriteFoodDep = new Tracker.Dependency;
-
-    var getFavoriteFood = function() {
-        favoriteFoodDep.depend();
-        return favoriteFood;
-    }
-
-    var setFavoriteFood = function(newValue) {
-        favoriteFood = newValue;
-        favoriteFoodDep.changed();
-    }
-
-    var getRandomNumber = function() {
-        favoriteFoodDep.depend();
-        return Math.random();
-    }
-
-    Template.favoriteFood.helpers({
-        "favoriteFood" : function() {
-            return getFavoriteFood();
-        },
-        "randomNumber": function() {
-            return getRandomNumber();
-        }
-    })
-
-    Template.favoriteFood.events({
-        "submit form": function(e) {
-            e.preventDefault();
-            setFavoriteFood(e.currentTarget.newFood.value);
-        }
-    })
+var getFavoriteFood = function() {
+    favoriteFoodDep.depend();
+    console.log('getFavoriteFood just ran.')
+    return favoriteFood;
 }
+
+var setFavoriteFood = function(newValue) {
+    favoriteFood = newValue;
+    favoriteFoodDep.changed();
+}
+
+var getRandomNumber = function() {
+    favoriteFoodDep.depend();
+    console.log('getRandomNumber just ran.');
+    return Math.random();
+}
+
+var handle = Tracker.autorun(function() {
+    // This will run immediately once at least when the app loads
+    // in the client due to it asking for the favorite food,
+    // which has been made into a reactive variable. It will run
+    // regardless of what route you are on.
+    console.log('Your favorite food is ' + getFavoriteFood());
+})
+
+Template.favoriteFood.helpers({
+    "favoriteFood" : function() {
+        return getFavoriteFood();
+    },
+    "randomNumber": function() {
+        return getRandomNumber();
+    }
+});
+
+Template.favoriteFood.events({
+    "submit form": function(e) {
+        e.preventDefault();
+        setFavoriteFood(e.currentTarget.newFood.value);
+    }
+});
